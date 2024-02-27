b3c10d72f4333a1e1b6a6b796d1893a6cccefc14
naathaliie <nathalielarsson@live.se>
2024-02-27 11:47:33 +0100

    DropDownMeny is working, yaas

--- src/components/DropDownMenu/DropDownMenu.jsx
+++ src/components/DropDownMenu/DropDownMenu.jsx
@@ -1,27 +1,38 @@
 import InfoBox from "../InfoBox/InfoBox";
 import charactersJson from "../json/characteres.json";
-import { useState } from "react"; 
+import { useState } from "react";
 
 const DropDownMenu = () => {
+  /* useState so we can collect the specific character that the user clicks on. 
+  Its default value is set to null (has no value) because no choice has been made untill the user makes one */
+  const [selectedCharacter, setSelectedCharacter] = useState(null);
 
-    const [selectedName, setSelectedName] = useState(null);
+  /* What will happen on the event onChange */
+  const handleOnChange = (event) => {
+    const selectedName = event.target.value; //Targets the value of the event
+    //.find() will search the charactersJson after a match of the selected name
+    const character = charactersJson.find(
+      (char) => char.firstname === selectedName
+    );
 
-    /* What will happen on the event onChange */
-    const handleOnChange = (event) => {
-        const selectedName = event.target.value;
-        console.log("Du klickade på", selectedName);
-        setSelectedName(selectedName);
-};
-
+    //updates the state of "selectedCharacter" to the found character
+    setSelectedCharacter(character);
+  };
 
   return (
     <div className="dropDownMenyDiv">
+      {/* Lable for the dropdown menu, htmlFor is needed to connect the lable with right tag */}
       <label htmlFor="option-select">Välj en karaktär</label>
 
+      {/* Row33: select-tag (is like a dropdown). id is needed if you have a label, this makes you connect it with the right lable.  
+          Row34: The first option, we don´t give this a value since its just information to the user.
+          Row35: The array.map() to go through every element.
+          Row36-42: For every element we want to create a new option to the select-tag. And to use the information later 
+          we set the value to someting unik for this element, like the firstname. And also we want to output 
+          to the user the characters foirstname and lastname (row 37)*/}
       <select name="characters" id="option-select" onChange={handleOnChange}>
         <option value="">--Välj en karaktär--</option>
         {charactersJson.map((caracter, index) => {
-          console.log();
           return (
             <option
               key={`${index}`}
@@ -31,7 +42,8 @@ const DropDownMenu = () => {
         })}
       </select>
 
-      {selectedName && <InfoBox selectedName={selectedName}/>}
+        {/* IF selectedCharacter isn´t null (its default value) we will call the komponent/function infobox and send the selectedCharacter to it */}
+      {selectedCharacter && <InfoBox character={selectedCharacter} />}
     </div>
   );
 };
--- src/components/InfoBox/InfoBox.jsx
+++ src/components/InfoBox/InfoBox.jsx
@@ -1,5 +1,21 @@
-const InfoBox = ({selectedName}) => {
-    return <h1>INFOBOX! {selectedName}</h1>
+const InfoBox = ({ character }) => {
+
+  /*The method Object.values() will return you an array of the values (värden) from an object. 
+   Each value corresponds to a property(egenskap) in the object. A property consists of a key and a value. 
+   Same with Object.keys(). It will return an array of the keys */
+  const valuestoPrint = Object.values(character);
+  const keysToPrint = Object.keys(character);
+
+  return (
+    <>
+      <h1>INFOBOX!</h1>
+      <ul className="infoBoxList">
+        {keysToPrint.map((key, index) => (
+          <li key={index}>{key}: {valuestoPrint[index]}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default InfoBox;
