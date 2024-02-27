db657279c7b7a3ce9fb8915a8ce67471e0d3b717
naathaliie <nathalielarsson@live.se>
2024-02-27 11:52:39 +0100

    InfoBox is finished

--- src/components/InfoBox/InfoBox.jsx
+++ src/components/InfoBox/InfoBox.jsx
@@ -1,17 +1,20 @@
 const InfoBox = ({ character }) => {
-
-  /*The method Object.values() will return you an array of the values (värden) from an object. 
+  /*The method Object.values() will return an array of ONLY the values (värden) from the object. 
    Each value corresponds to a property(egenskap) in the object. A property consists of a key and a value. 
-   Same with Object.keys(). It will return an array of the keys */
+   Same with Object.keys(). It will return an array of ONLY the keys */
   const valuestoPrint = Object.values(character);
   const keysToPrint = Object.keys(character);
+  console.log(keysToPrint);
+  console.log(valuestoPrint);
 
   return (
     <>
       <h1>INFOBOX!</h1>
       <ul className="infoBoxList">
         {keysToPrint.map((key, index) => (
-          <li key={index}>{key}: {valuestoPrint[index]}</li>
+          <li key={index}>
+            {key}: {valuestoPrint[index]}
+          </li>
         ))}
       </ul>
     </>
